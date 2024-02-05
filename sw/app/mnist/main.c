e3a5569fbb8a03e861afb2f1cd00cf0819a3e981
raffael-daltoe <raffel_rocha@hotmail.com>
2024-02-05 23:06:20 +0100

    Access by Write_csr and read_csr

--- sw/app/mnist/main.c
+++ sw/app/mnist/main.c
@@ -7,20 +7,23 @@
 #include "Network.h"
 #include "util.h"
 
+// #define mhpmcounterh1 0xB83
+// #define mhpmcounterh2 0xB84
+
 void readStimulus(
-                  UDATA_T* inputBuffer,
-                  Target_T* expectedOutputBuffer)
+    UDATA_T *inputBuffer,
+    Target_T *expectedOutputBuffer)
 {
-    envRead(ENV_SIZE_Y*ENV_SIZE_X*ENV_NB_OUTPUTS,
+    envRead(ENV_SIZE_Y * ENV_SIZE_X * ENV_NB_OUTPUTS,
             ENV_SIZE_Y, ENV_SIZE_X,
-            (DATA_T*) inputBuffer, //TODO
+            (DATA_T *)inputBuffer, // TODO
             OUTPUTS_SIZE[0], expectedOutputBuffer);
 }
 
-int processInput(        UDATA_T* inputBuffer,
-                            Target_T* expectedOutputBuffer,
-                            Target_T* predictedOutputBuffer,
-			    UDATA_T* output_value)
+int processInput(UDATA_T *inputBuffer,
+                 Target_T *expectedOutputBuffer,
+                 Target_T *predictedOutputBuffer,
+                 UDATA_T *output_value)
 {
     size_t nbPredictions = 0;
     size_t nbValidPredictions = 0;
@@ -28,28 +31,35 @@ int processInput(        UDATA_T* inputBuffer,
     propagate(inputBuffer, predictedOutputBuffer, output_value);
 
     // assert(expectedOutputBuffer.size() == predictedOutputBuffer.size());
-    for(size_t i = 0; i < OUTPUTS_SIZE[0]; i++) {
-        if (expectedOutputBuffer[i] >= 0) {
+    for (size_t i = 0; i < OUTPUTS_SIZE[0]; i++)
+    {
+        if (expectedOutputBuffer[i] >= 0)
+        {
             ++nbPredictions;
 
-            if(predictedOutputBuffer[i] == expectedOutputBuffer[i]) {
+            if (predictedOutputBuffer[i] == expectedOutputBuffer[i])
+            {
                 ++nbValidPredictions;
             }
         }
     }
 
     return (nbPredictions > 0)
-        ? nbValidPredictions : 0;
+               ? nbValidPredictions
+               : 0;
 }
 
-
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
     // const N2D2::Network network{};
     size_t instret, cycles;
+    volatile unsigned int cache_I, cache_D=0;
 
+    write_csr(0xB03,0x0);    // mhpmcounter3 = 0
+    write_csr(0xB04,0x0);    // mhpmcounter4 = 0
 #if ENV_DATA_UNSIGNED
-    UDATA_T inputBuffer[ENV_SIZE_Y*ENV_SIZE_X*ENV_NB_OUTPUTS];
+    UDATA_T inputBuffer[ENV_SIZE_Y * ENV_SIZE_X * ENV_NB_OUTPUTS];
 #else
     std::vector<DATA_T> inputBuffer(network.inputSize());
 #endif
@@ -61,12 +71,18 @@ int main(int argc, char* argv[]) {
     readStimulus(inputBuffer, expectedOutputBuffer);
     instret = -read_csr(minstret);
     cycles = -read_csr(mcycle);
-    const int success = processInput(inputBuffer, 
-                                                        expectedOutputBuffer, 
-                                                        predictedOutputBuffer,
-							&output_value);
+    cache_I = read_csr(0xB03);
+    cache_D = read_csr(0xB04);
+
+    const int success = processInput(inputBuffer,
+                                     expectedOutputBuffer,
+                                     predictedOutputBuffer,
+                                     &output_value);
+
     instret += read_csr(minstret);
     cycles += read_csr(mcycle);
+    cache_I += read_csr(0xB03);
+    cache_D += read_csr(0xB04);
     
     printf("Expected  = %d\n", expectedOutputBuffer[0]);
     printf("Predicted = %d\n", predictedOutputBuffer[0]);
@@ -74,12 +90,14 @@ int main(int argc, char* argv[]) {
     printf("credence: %d\n", output_value);
     printf("image %s: %d instructions\n", stringify(MNIST_INPUT_IMAGE), (int)(instret));
     printf("image %s: %d cycles\n", stringify(MNIST_INPUT_IMAGE), (int)(cycles));
-
+    printf("Cache Miss Instructions = [%d]\nCache Miss Data = [%d]\n", cache_I, cache_D);
+    
 #ifdef OUTPUTFILE
     FILE *f = fopen("success_rate.txt", "w");
-    if (f == NULL) {
+    if (f == NULL)
+    {
         N2D2_THROW_OR_ABORT(std::runtime_error,
-            "Could not create file:  success_rate.txt");
+                            "Could not create file:  success_rate.txt");
     }
     fprintf(f, "%f", successRate);
     fclose(f);
