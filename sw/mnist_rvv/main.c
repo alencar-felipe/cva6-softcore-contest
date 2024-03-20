442d5eb5374a88312e58de906f56cbadd7c3f063
alencar-felipe <alencar.felipe.paiva@gmail.com>
2024-03-20 01:02:54 +0100

    example code on mnist_rvv (spike not working, triggers exception on second instruction in cva6)

--- sw/mnist_rvv/main.c
+++ sw/mnist_rvv/main.c
@@ -3,8 +3,16 @@
 #include <string.h>
 #include <stdint.h>
 
+#include <riscv_vector.h>
+
 #include "network.h"
 #include "resources/all.h"
+
+#define VL (4)
+
+int32_t a_mem[VL] = {0, 1, 2, 3};
+int32_t b_mem[VL] = {4, 5, 6, 7};
+int32_t c_mem[VL] = {9, 9, 9, 9};
 
 int main() {
 
@@ -15,6 +23,20 @@ int main() {
 #ifdef VALIDATION_RUN
     printf(TAG "Validation Run\n");
 #endif
+
+    ASSERT(__riscv_vsetvl_e32m1(VL) == VL);
+
+    vint32m1_t a, b, c;
+
+    a = __riscv_vle32_v_i32m1(a_mem, VL);
+    b = __riscv_vle32_v_i32m1(b_mem, VL);
+    c = __riscv_vadd_vv_i32m1(a, b, VL);
+    __riscv_vse32_v_i32m1(c_mem, c, VL);
+
+    for (size_t i = 0; i < VL; i++) {
+        printf("%d ", c_mem[i]);
+    }
+    printf("\n");
 
     uint8_t *input;
     int32_t expected;
